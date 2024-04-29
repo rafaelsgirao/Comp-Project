55c98292bb5f85ff424485c7da4502fffa74aed8
Guilherme Belchior <guilherme.belchior.souza@tecnico.ulisboa.pt>
2024-04-29 15:41:57 +0100

    fix: Change alloc node to be a unary_expression_node

--- ast/alloc_node.h
+++ ast/alloc_node.h
@@ -1,20 +1,18 @@
 #ifndef __TIL_AST_ALLOC_NODE_H__
 #define __TIL_AST_ALLOC_NODE_H__
 
-#include <cdk/ast/expression_node.h>
+#include <cdk/ast/unary_operation_node.h>
 
 namespace til {
 
   /**
    * Class for describing alloc nodes.
    */
-  class alloc_node : public cdk::expression_node {
-    cdk::expression_node *_arg;
-
+  class alloc_node : public cdk::unary_operation_node {
 
   public:
     alloc_node(int lineno, cdk::expression_node *arg) :
-        cdk::expression_node(lineno) , _arg(arg) {
+        cdk::unary_operation_node(lineno, arg) {
     }
 
 
